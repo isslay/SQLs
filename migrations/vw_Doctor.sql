74558e81c58a1caedf12023002b718d8a6405947
isslay <154976543@qq.com>
2024-12-09 01:01:34 +0800

    add 2 views

--- migrations/vw_Doctor.sql
+++ migrations/vw_Doctor.sql
@@ -1,4 +1,16 @@
-CREATE VIEW vw_TeYaoTong_Doctors AS
+CREATE VIEW vw_teyaotong_products AS
+select DISTINCT id FROM pharma
+
+CREATE VIEW vw_teyaotong_doctors AS
 select a.id, a.name, a.dept_name 
 from sys_user a INNER JOIN sys_user_role b on a.id =b.user_id and a.state =0 and b.state =0 
 where b.role_id=3025;
+
+
+CREATE VIEW vw_teyaotong_insurance AS
+select id,name from dict where parent_id =1010 and state=0;
+
+
+CREATE VIEW vw_teyaotong_form AS
+select id,name from dict where parent_id =1027 and state=0;
+
